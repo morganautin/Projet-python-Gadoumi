fa2167e083bee43aff60b4771600cf9ef85a7241
Morgan Autin <morgan.autin@gmail.com>
2025-11-05 16:12:12 +0100

    feat(models): ajoute Category + lie Product.category + champ stock

--- products/models.py
+++ products/models.py
@@ -1,8 +1,31 @@
 from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(max_length=140, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=120, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)  # nouveau
+    category = models.ForeignKey(
+        Category, null=True, blank=True, on_delete=models.SET_NULL,
+        related_name="products"
+    )  # nouveau
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
