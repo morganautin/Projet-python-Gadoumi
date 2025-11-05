88b3b1d689bf4749265ca7525f36aa2565014044
Morgan Autin <morgan.autin@gmail.com>
2025-11-05 16:17:11 +0100

    feat(api): ajoute ProductFilter (min/max price, category slug, in_stock)

---
+++ products/filters.py
@@ -0,0 +1,16 @@
+import django_filters
+from .models import Product
+
+
+class ProductFilter(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+    category = django_filters.CharFilter(field_name="category__slug", lookup_expr="exact")
+    in_stock = django_filters.BooleanFilter(method="filter_in_stock")
+
+    class Meta:
+        model = Product
+        fields = ["category", "min_price", "max_price", "in_stock"]
+
+    def filter_in_stock(self, queryset, name, value: bool):
+        return queryset.filter(stock__gt=0) if value else queryset
